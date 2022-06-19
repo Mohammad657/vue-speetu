f6b104c702daa3ec7b4455ed9c4d12c02893b880
mohammad1997a <alalaqm217@gmail.com>
2022-06-19 13:28:22 +0300

    support page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,11 @@
       <Header msg="Header" /> 
     <About msg="About" />
     <Service msg="Service" />
-    <!-- <Support msg="Support" /> -->
+    <Support msg="Support" />
     <!-- <OurProjects msg="OurProjects" /> -->
     <!-- <VideoBusiness msg="VideoBusiness" /> --> 
-    <WorkProcess title="P R O C E S S" subtitle="Work Process" />
-    <OurClients msg="OurClients" />
+    <!-- <WorkProcess title="P R O C E S S" subtitle="Work Process" />
+    <OurClients msg="OurClients" /> -->
     <!-- <Testimonials msg="Testimonials" /> -->
     <!-- <Partnership msg="Partnership" /> -->
     <!-- <RecentPosts msg="RecentPosts" /> -->
